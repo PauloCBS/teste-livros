b7fb0677ccad70bf2eab38626da6a9e63834138e
Paulo Cesar <paulo-cesar-batista@hotmail.com>
2021-09-30 21:49:40 -0300

    Foram adicionados a Barra superior e inferior

---
+++ src/components/shared/AppBottomBar.vue
@@ -0,0 +1,40 @@
+<template>
+<v-app>
+ <v-bottom-navigation
+    :value="value"
+    color="primary"
+  >
+    <v-btn>
+      <span>Recents</span>
+
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+  <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'AppBottomBar',
+
+  data: () => ({
+    value: 1
+  }),
+};
+</script>
