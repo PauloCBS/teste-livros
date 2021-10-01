39207b55429e8656b9e4c117906ebaa2252298f1
Paulo <prmanfredini@gmail.com>
2021-09-30 22:49:40 -0300

    fix app bottom

--- src/components/shared/AppBottomBar.vue
+++ src/components/shared/AppBottomBar.vue
@@ -1,28 +1,25 @@
 <template>
-<v-app>
- <v-bottom-navigation
+  <v-bottom-navigation
     :value="value"
-    color="primary"
+    color="teal"
+    grow
   >
     <v-btn>
       <span>Recents</span>
-
       <v-icon>mdi-history</v-icon>
     </v-btn>
 
     <v-btn>
       <span>Favorites</span>
-
       <v-icon>mdi-heart</v-icon>
     </v-btn>
-
+    
     <v-btn>
       <span>Nearby</span>
 
       <v-icon>mdi-map-marker</v-icon>
     </v-btn>
   </v-bottom-navigation>
-  </v-app>
 </template>
 
 <script>
--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -1,83 +1,49 @@
 <template>
-  <v-app>
+  <v-container>
       <v-app-bar
       color="deep-purple accent-4"
       app
       dark
-    >
+      >
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-toolbar-title>
         <v-avatar size="24">
             <v-img src="http://www.educacao.al.gov.br/images/logo-01.png" size="25"/>
         </v-avatar>
-       Doe um livro</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+       Doe um livro
+      </v-toolbar-title>
+      </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
         <v-list nav dense>
-        <v-list-item-group v-model="group">
-            
-            <router-link to="/">
-                <v-list-item>
-                    <v-list-item-icon>
-                        <v-icon>mdi-home</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>
-                        Início
-                    </v-list-item-title>
-                </v-list-item>
-            </router-link>
+          <v-list-item-group v-model="group">
+              
+              <router-link to="/">
+                  <v-list-item>
+                      <v-list-item-icon>
+                          <v-icon>mdi-home</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-title>
+                          Início
+                      </v-list-item-title>
+                  </v-list-item>
+              </router-link>
 
-            <router-link to="/about">
-                <v-list-item>
-                    <v-list-item-icon>
-                        <v-icon>mdi-information</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title>
-                        Segunda aba
-                    </v-list-item-title>
-                </v-list-item>
-            </router-link>
-            </v-list-item-group>
+              <router-link to="/about">
+                  <v-list-item>
+                      <v-list-item-icon>
+                          <v-icon>mdi-information</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-title>
+                          Segunda aba
+                      </v-list-item-title>
+                  </v-list-item>
+              </router-link>
+          </v-list-item-group>
         </v-list>
     </v-navigation-drawer>
-  </v-app>
+  </v-container>
 </template>
 
 <script>
